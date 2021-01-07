31f90b16add238d05a75c40eaab226bbc6632c8c
MariaGaro <s202378@student.dtu.dk>
2021-01-07 13:27:37 +0100

    final lib

--- maria/maria/matmult_lib.c
+++ maria/maria/matmult_lib.c
@@ -2,6 +2,6 @@
 
 void
 matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
-  cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,m,n,k,1.,&A[0][0],m,&B[0][0],k,0.,&C[0][0],m);
+  cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,m,n,k,1.,&A[0][0],k,&B[0][0],n,0.,&C[0][0],n);
 }
 
