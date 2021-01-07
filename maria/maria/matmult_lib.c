5418af742ac48c76ca8a235f3ff5381cda7aec4f
MariaGaro <s202378@student.dtu.dk>
2021-01-07 11:04:49 +0100

    change order

--- maria/maria/matmult_lib.c
+++ maria/maria/matmult_lib.c
@@ -1,7 +1,7 @@
 #include "cblas.h"
 
 void
-matmult_lib(int m, int k, int n, double **A, double **B, double **C) {
+matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
   cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,m,n,k,1.,&A[0][0],m,&B[0][0],k,0.,&C[0][0],m);
 }
 
--- maria/maria/matmult_nat.h
+++ maria/maria/matmult_nat.h
@@ -1,5 +1,5 @@
 #ifndef __MATMULT_NAT_H
 #define __MATMULT_NAT_H
 
-void matmult_nat(int m, int k, int n, double **A, double **B, double **C);
+void matmult_nat(int m, int n, int k, double **A, double **B, double **C);
 #endif
